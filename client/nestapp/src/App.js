75be26b1ae0bcf7d10929895d903df450a359e4f
Garoqui <quiroga_alejandro@hotmail.com>
2021-06-15 16:49:06 -0500

    create react app

---
+++ client/nestapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Hello } from './components/services'
+
+function App() {
+  return (
+    <div className="App">
+        <Hello/>
+        
+    </div>
+  );
+}
+
+export default App;
